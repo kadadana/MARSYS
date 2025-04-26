35ffb568f0ecb8d07699baf7ee47a53fd14c10e2
Kadir Yılmaz <kadiryilmaz0136@gmail.com>
2025-04-26 18:37:49 +0300

    User id ve isim gözükmeme sorunu giderildi

--- src/main/java/com/marsys/marsys/Controllers/MainController.java
+++ src/main/java/com/marsys/marsys/Controllers/MainController.java
@@ -1,11 +1,13 @@
 package com.marsys.marsys.Controllers;
 
+
 import com.almasb.fxgl.entity.action.Action;
 import com.marsys.marsys.Models.Employee;
 import com.marsys.marsys.Models.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,16 +17,27 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     LayoutController _layoutController = new LayoutController();
     @FXML
     private Button btnLogout;
     @FXML
     private Button goToSalesScreen;
-
+    @FXML
+    private Label lblUserName;
+    @FXML
+    private Label lblUserId;
 
     Employee user = Session.getInstance().getCurrentUser();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        lblUserName.setText(user.getFirstName() + " " + user.getLastName());
+        lblUserId.setText("ID: " + user.getId());
+    }
 
     public void goToSalesScreen(ActionEvent event) {
         if (user.getPosition().equals("MANAGER") || user.getPosition().equals("CASHIER")) {
