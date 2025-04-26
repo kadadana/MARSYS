63ef72785f2d737a6cdd398c725e45f3e67877d6
Kadir Yılmaz <kadiryilmaz0136@gmail.com>
2025-04-26 18:36:18 +0300

    Ödeme toplamı yansımama sorunu çözüldü

--- src/main/java/com/marsys/marsys/Controllers/PaymentModalController.java
+++ src/main/java/com/marsys/marsys/Controllers/PaymentModalController.java
@@ -2,15 +2,17 @@ package com.marsys.marsys.Controllers;
 
 import com.marsys.marsys.Repository.Repository;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.util.concurrent.Callable;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PaymentModalController {
+public class PaymentModalController implements Initializable {
     Repository _repository = new Repository();
     @FXML
     private TextField cardNumber;
@@ -18,45 +20,59 @@ public class PaymentModalController {
     private PasswordField cardPassword;
     @FXML
     private Label lblTotal;
+    private double paymentTotal;
 
     private PaymentCompleteListener paymentCompleteListener;
-    public double paymentTotal;
-
+    public void setPaymentTotal(double total) {
+        this.paymentTotal = total;
+        lblTotal.setText(total + " TL"); // burada lblTotalPayment, ekrandaki label ise
+    }
     public void setPaymentCompleteListener(PaymentCompleteListener listener) {
         this.paymentCompleteListener = listener;
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setPaymentTotal(paymentTotal);
+    }
+
     @FXML
     private void handlePayment() {
-        if (cardNumber.getText().equals(_repository.getCardInfo("NUMBER", "NUMBER", cardNumber.getText())) &&
-                cardPassword.getText().equals(_repository.getCardInfo("PASSWORD", "PASSWORD", cardPassword.getText()))) {
-            if (paymentTotal <= Double.parseDouble(_repository.getCardInfo("BALANCE", "NUMBER", cardNumber.getText()))) {
-                if (paymentCompleteListener != null) {
-                    paymentCompleteListener.onPaymentComplete(cardNumber.getText());
+        try {
+            if (cardNumber.getText().equals(_repository.getCardInfo("NUMBER", "NUMBER", cardNumber.getText())) &&
+                    cardPassword.getText().equals(_repository.getCardInfo("PASSWORD", "PASSWORD", cardPassword.getText())) &&
+                    (_repository.getCardInfo("BALANCE", "NUMBER", cardNumber.getText())) != null) {
+                if (paymentTotal <= Double.parseDouble(_repository.getCardInfo("BALANCE", "NUMBER", cardNumber.getText()))) {
+                    if (paymentCompleteListener != null) {
+                        paymentCompleteListener.onPaymentComplete(cardNumber.getText());
+                    }
+                    _repository.reduceCardBalance(paymentTotal, cardNumber.getText());
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information");
+                    alert.setHeaderText("Completed");
+                    alert.setContentText("Sale is completed!");
+                    alert.showAndWait();
+                    closeModal();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Warning");
+                    alert.setHeaderText("Not Completed");
+                    alert.setContentText("The card entered has not enough balance for this transaction!");
+                    alert.showAndWait();
                 }
-                _repository.reduceCardBalance(paymentTotal, cardNumber.getText());
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Information");
-                alert.setHeaderText("Completed");
-                alert.setContentText("Sale is completed!");
-                alert.showAndWait();
-                closeModal();
+
             } else {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Warning");
-                alert.setHeaderText("Not Completed");
-                alert.setContentText("The card entered has not enough balance for this transaction!");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Invalid card");
+                alert.setContentText("Card number or password is incorrect.");
                 alert.showAndWait();
+
             }
-
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Invalid card");
-            alert.setContentText("Card number or password is incorrect.");
-            alert.showAndWait();
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
 
     }
 
