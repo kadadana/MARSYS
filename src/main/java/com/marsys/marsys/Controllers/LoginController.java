6faff6de0680ef2dfa209e799ce8afa7bd20d74c
Kadir Yılmaz <kadiryilmaz0136@gmail.com>
2025-04-26 15:45:26 +0300

    Login ekraında enter tuşuyla giriş eklendi

--- src/main/java/com/marsys/marsys/Controllers/LoginController.java
+++ src/main/java/com/marsys/marsys/Controllers/LoginController.java
@@ -7,18 +7,22 @@ import eu.hansolo.toolbox.unit.Converter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
-
     Repository _repository = new Repository();
+    LayoutController _layoutController = new LayoutController();
+
 
     @FXML
     private TextField storeCodeField;
@@ -26,15 +30,25 @@ public class LoginController {
     private TextField idField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Button btnLogin;
 
     @FXML
-    public void handleLogin(ActionEvent event) {
+    private void initialize() {
+        // Enter'a basınca login olsun
+        storeCodeField.setOnAction(event -> handleLogin());
+        idField.setOnAction(event -> handleLogin());
+        passwordField.setOnAction(event -> handleLogin());
+    }
+
+    @FXML
+    public void handleLogin() {
 
         String enteredStoreCode = storeCodeField.getText();
         String enteredId = idField.getText();
         String enteredPassword = passwordField.getText();
         String password = _repository.getCellById("EMPLOYEE", "PASSWORD", enteredId);
-        String storeCode = _repository.getCellById("EMPLOYEE","STORE_CODE",enteredId);
+        String storeCode = _repository.getCellById("EMPLOYEE", "STORE_CODE", enteredId);
 
 
         if (enteredPassword.equals(password) && enteredStoreCode.equals(storeCode)) {
@@ -42,9 +56,11 @@ public class LoginController {
             String name = _repository.getCellById("EMPLOYEE", "NAME", enteredId);
             String lastName = _repository.getCellById("EMPLOYEE", "LAST_NAME", enteredId);
             String position = _repository.getCellById("EMPLOYEE", "POSITION", enteredId);
-            Employee loggedInEmployee = new Employee(name, lastName, position,id, password, storeCode);
+            Employee loggedInEmployee = new Employee(name, lastName, position, id, password, storeCode);
             Session.getInstance().setCurrentUser(loggedInEmployee);
-            showNextScene();
+            _layoutController.loadPageByButton("/com/marsys/marsys/Views/mainpage.fxml", btnLogin);
+
+
         } else {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Log in failed");
@@ -54,24 +70,5 @@ public class LoginController {
         }
     }
 
-    private void showNextScene() {
-        Stage stage = (Stage) idField.getScene().getWindow();
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/marsys/marsys/Views/mainpage.fxml"));
-            Scene newScene = new Scene(fxmlLoader.load(), 720, 720);
-            stage.setScene(newScene);
-            stage.setTitle("Main Page");
-        } catch (IOException e) {
-            showErrorMessage("Error Loading Next Scene", "There was an error loading the next screen. Please try again.");
-            e.printStackTrace();
-        }
-    }
 
-    private void showErrorMessage(String headerText, String contentText) {
-        Alert alert = new Alert(AlertType.ERROR);
-        alert.setTitle("Log in failed");
-        alert.setHeaderText(headerText);
-        alert.setContentText(contentText);
-        alert.showAndWait();
-    }
 }
