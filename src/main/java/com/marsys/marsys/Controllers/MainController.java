cd11c74e7c2852d1405a70d643aa7fa165af184a
Kadir Yılmaz <kadiryilmaz0136@gmail.com>
2025-04-27 19:42:39 +0300

    Kampanya ekranına gidiş için tuş eklendi

--- src/main/java/com/marsys/marsys/Controllers/MainController.java
+++ src/main/java/com/marsys/marsys/Controllers/MainController.java
@@ -1,22 +1,15 @@
 package com.marsys.marsys.Controllers;
 
 
-import com.almasb.fxgl.entity.action.Action;
 import com.marsys.marsys.Models.Employee;
 import com.marsys.marsys.Models.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,11 +18,13 @@ public class MainController implements Initializable {
     @FXML
     private Button btnLogout;
     @FXML
-    private Button goToSalesScreen;
+    private Button btnGoToSalesScreen;
     @FXML
     private Label lblUserName;
     @FXML
     private Label lblUserId;
+    @FXML
+    private Button btnGoToDiscountCampaignScreen;
 
     Employee user = Session.getInstance().getCurrentUser();
 
@@ -39,20 +34,16 @@ public class MainController implements Initializable {
         lblUserId.setText("ID: " + user.getId());
     }
 
-    public void goToSalesScreen(ActionEvent event) {
+    public void goToSalesScreen() {
         if (user.getPosition().equals("MANAGER") || user.getPosition().equals("CASHIER")) {
 
-            _layoutController.loadPageByButton("/com/marsys/marsys/Views/sales.fxml", goToSalesScreen);
-
-
+            _layoutController.loadPageByButton("/com/marsys/marsys/Views/sales.fxml", btnGoToSalesScreen);
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setContentText("You don't have access for this operation!");
             alert.showAndWait();
         }
-
-
     }
 
     public void goToProductEntryScreen() {
@@ -76,7 +67,15 @@ public class MainController implements Initializable {
     }
 
     public void goToDiscountCampaignScreen() {
-        // Burada sales screen'e gitme işlemini yapabilirsiniz.
+        if (user.getPosition().equals("MANAGER")) {
+
+            _layoutController.loadPageByButton("/com/marsys/marsys/Views/discountandcampaign.fxml", btnGoToDiscountCampaignScreen);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("You don't have access for this operation!");
+            alert.showAndWait();
+        }
     }
 
     public void logout(ActionEvent event) {
